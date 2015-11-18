5d017cd0aa92b06761758cb1bd82e1b3dae92a89
minhhn2910 <minhhn2910@gmail.com>
2015-11-18 11:14:47 +0800

    clean code

--- PrecisionAnalysis/exponentTracking.py
+++ PrecisionAnalysis/exponentTracking.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import sys
 import subprocess
+import numpy as np
+from functools import partial
 def parse_output(line):
 	list_target = []
 	array = line.replace(' ', '').replace('\n', '').split(',')
@@ -21,9 +23,17 @@ def main(argv):
 	for i in range(num_testcases):
 		output = parse_output(subprocess.Popen([program, '%s'%(i)],stdout=subprocess.PIPE).communicate()[0])
 		result.append(output)
-	#print zip (*result)	
-	print "min avg exponent" + str(map(min,zip(*result)))
-	print "max avg exponent" + str(map(max,zip(*result)))
-
+	
+	# np.percentile(sqnr_vector, PERCENTILE_VALUE)
+	mapfunc20p = partial(np.percentile, q = 95)
+	mapfunc80p = partial(np.percentile, q = 5)
+	
+	#print zip (*result)[0]
+	#print zip(*result)
+	print "5%% percentile " + str(map(int,map(mapfunc20p,zip(*result))))
+	print "95%% percentile " + str(map(int,map(mapfunc80p,zip(*result))))
+	print "min avg exponent " + str(map(min,zip(*result)))
+	print "max avg exponent " + str(map(max,zip(*result)))
+#SQNR_pecentile = np.percentile(sqnr_vector, PERCENTILE_VALUE)	
 if __name__ == '__main__':
     main(sys.argv[1:])	
