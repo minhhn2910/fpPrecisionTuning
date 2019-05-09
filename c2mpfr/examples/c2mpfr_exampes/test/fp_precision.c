0bf78e7a6ba0c905a5bbc9661859cdea0e2312c8
test <minhhn2910@gmail.com>
2019-05-10 02:32:12 +0800

    add test example

---
+++ c2mpfr/examples/c2mpfr_exampes/test/fp_precision.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   float a, b, c, d;
+   d = (a+b)*c;
+   return 0;
+}
+                                                                                               
---
+++ c2mpfr/examples/c2mpfr_exampes/test/fp_precision_modified.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   float a = 0.123456789012, b= 1.234567890123, c=2.3456789012345 , d;
+   d = (a+b)*c;
+   printf("%.8f,", d);
+   return 0;
+}
+                                                                                               
